dc3e3f950ecc990e130ba0bfc44170edddb87409
Adrian Gierakowski <agierakowski@gmail.com>
2023-07-20 16:15:42 +0100

    improve process-compose wrapper to allow calling sub-commands other than up
    tui and config file are set via env vars, which do not trigger errors for
    sub-commands which do not expect corresponding command line flags
    
    port is still set vai command line arguments since it is the only global one

--- nix/process-compose/default.nix
+++ nix/process-compose/default.nix
@@ -46,17 +46,14 @@ in
       runtimeInputs = [ config.package ];
       text = ''
         ${if config.debug then "cat ${config.outputs.settingsYaml}" else ""}
-        exec process-compose \
-          ${if config.port != null then "-p ${toString config.port}\\\n" else ""} "$@"
-      '';
-    };
-    package-up = pkgs.writeShellApplication {
-      name = "${name}-up";
-      text = ''
-        exec ${lib.getExe config.outputs.package} up \
-          -f ${config.outputs.settingsYaml} \
-          ${config.outputs.upCommandArgs} \
-          "$@"
+        export PC_CONFIG_FILES=${config.outputs.settingsYaml}
+        ${
+          # Once the following issue is fixed we should be able to simply do:
+          # export PC_DISABLE_TUI=${builtins.toJSON (!config.tui)}
+          # https://github.com/F1bonacc1/process-compose/issues/75
+          if config.tui then "" else "export PC_DISABLE_TUI=true"
+        }
+        exec process-compose -p ${toString config.port} "$@"
       '';
     };
   };
